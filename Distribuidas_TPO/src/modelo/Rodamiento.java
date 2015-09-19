39a62fd002af65208f43abfdfec7e73ce58f4af7
alnocetti <alnocetti@gmail.com>
2015-09-19 16:40:17 -0300

    prueba ssubida rodamiento

--- Distribuidas_TPO/src/modelo/Rodamiento.java
+++ Distribuidas_TPO/src/modelo/Rodamiento.java
@@ -1,5 +1,59 @@
 package modelo;
 
 public class Rodamiento {
-int codigo;
+	private int codigo;
+	private int sfk;
+	private String origen;
+	private String marca;
+	
+	
+	public Rodamiento(int codigo, int sfk, String origen, String marca) {
+		super();
+		this.codigo = codigo;
+		this.sfk = sfk;
+		this.origen = origen;
+		this.marca = marca;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public int getSfk() {
+		return sfk;
+	}
+
+
+	public void setSfk(int sfk) {
+		this.sfk = sfk;
+	}
+
+
+	public String getOrigen() {
+		return origen;
+	}
+
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	
 }
